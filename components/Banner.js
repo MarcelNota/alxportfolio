ff00da62ab581d86603132659f25454d17ad1db9
MarcelNota <n3gr1t0.mn@gmail.com>
2025-01-25 17:06:23 +0200

    addition of dependecys

---
+++ components/Banner.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import styles from "./banner.module.css";
+
+export default function Banner(){
+    return(
+        <div className="w-full">
+            <div className={`${styles.banner} h-screen`}>
+
+            </div>
+        </div>
+    )
+}
